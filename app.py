e03845fac00bb8b109e43b020fa13ccdc8355b96
MariskaS85 <mcfsnijdewind@gmail.com>
2020-12-11 15:42:52 +0000

    funtion update number and roll dice not working

--- app.py
+++ app.py
@@ -19,12 +19,26 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-#render about page
+#render index page
 @app.route("/")
 @app.route("/index")
 def index():
     naam = mongo.db.naam.find()
     return render_template("index.html", naam=naam)
+
+@app.route("/update_number")
+def update_number(naam_id):
+    number = mongo.db.naam.find_one({_id: ObjectId(naam_id)})
+    if request.method == "POST":
+        naam_dict = {
+            "eerstegooi": request.form.get("eerstegooi")
+        }
+        mongo.db.naam.update({"_id": ObjectId(naam_id)}, naam_dict)
+        
+    return redirect(url_for("index"))
+
+
+
 
 if __name__=="__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,19 @@
     $('select').formSelect();
     $('input#input_text, textarea#textarea2').characterCounter();
   });
+
+function roll() {
+    var result = Math.floor(Math.random() * 6 + 1);
+    $('result').text("Je gooide een" + result + "!");
+}
+
+dice.onclick = function () {
+    roll();
+};
+
+
+   
+
   
   /*
   Collapse sidenav on small devices
